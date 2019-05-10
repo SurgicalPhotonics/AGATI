1026866f4afb75eb14679d922463ee412fda393c
Nathaniel Adamian <nat.adamian241@gmail.com>
2019-05-10 15:31:38 -0400

    Tracker class updates
    Tracker is now a class so data can be stored as class variable.

--- Tracker.py
+++ Tracker.py
@@ -3,6 +3,7 @@ from typing import List
 from TrackingObjects import Point
 from TrackingObjects import Line
 from math import sqrt, asin
+from DataReader import read_data
 
 """bp = List[Point]  # list of all marked parts presumably get with frame
 left_cord = Line(bp[0], bp[7])
@@ -18,6 +19,30 @@ right vocal process. Camera flips the image so patient's left is
 our right"""
 
 
+class Tracker:
+    """Creates a tracker object that takes a dataset and can perform various
+    calculations on it.
+    data: lst[lst[float]]
+    contains marked parts and their corresponding points at each frame.
+    """
+    def __init__(self, data):
+        self.data = data
+
+    def angle_of_opening(self, ac1, ac2, left_cord, right_cord):
+        """Uses midline defined by points around anterior commissure to
+        approximate angle of opening on each side."""
+        midline = Line(ac1, ac2)
+        top_left = left_cord[len(left_cord)]
+        top_right = right_cord[len(right_cord)]
+        left_line = Line(ac1, top_left)
+        right_line = Line(ac1, top_right)
+        left_opp = shortest_distance(top_left, midline)
+        right_opp = shortest_distance(top_right, midline)
+        return (asin(left_opp / line_len(left_line)), asin(right_opp /
+                                                           line_len(
+                                                               right_line)))
+
+
 def line_len(l: Line):
     """Returns length of input line"""
     return sqrt((l.end2.x - l.end1.x) ^ 2 + (l.end2.y - l.end1.y) ^ 2)
@@ -28,18 +53,6 @@ def shortest_distance(p, l):
     return abs((l.slope * p.x + -1 * p.y + l.yint)) / (sqrt(l.slope * l.slope
                                                             + 1))
 
-def angle_of_opening(ac1, ac2, left_cord, right_cord):
-    """Uses midline defined by points around anterior commissure to approximate
-    angle of opening on each side."""
-    midline = Line(ac1, ac2)
-    top_left = left_cord[len(left_cord)]
-    top_right = right_cord[len(right_cord)]
-    left_line = Line(ac1, top_left)
-    right_line = Line(ac1, top_right)
-    left_opp = shortest_distance(top_left, midline)
-    right_opp = shortest_distance(top_right, midline)
-    return [asin(left_opp / line_len(left_line)), asin(right_opp /
-                                                       line_len(right_line))]
 
 
 
