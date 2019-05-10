5f83af26033c8ee60c827f5da6dba5b14130205f
Nathaniel Adamian <nat.adamian241@gmail.com>
2019-05-10 09:53:50 -0400

    Updated data_reader and added data files
    .h5 files containing DLC output data added to repo. DataReader uses dicts to take in.

--- DataReader.py
+++ DataReader.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 # might turn into class later
-filename = 'file.h5' # will either pass in individually or read from list
-data = np.array(pd.read_hdf(filename, 'df_with_missing'))
-"""above line is incorrect right now but I can't see how the data is stored in 
-the file until the network is done training so I'll fix it then"""
+filename = 'vocal0DeepCut_resnet50_vocal_strobeMay8shuffle1_200000.h5'
+placeholder = pd.read_hdf(filename)
+data = placeholder['DeepCut_resnet50_vocal_strobeMay8shuffle1_200000']
+
