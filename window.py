8fb6f3fe7db134bb330510b9e405c83d47c48cae
Nathaniel Adamian <nat.adamian241@gmail.com>
2019-05-17 15:48:14 -0400

    Bare essentials of wx window implemented

--- window.py
+++ window.py
@@ -4,11 +4,34 @@ import DataReader
 import TrackingObjects
 from tracker import Tracker
 
+
+class Window(wx.Frame):
+    """Window object that we'll use as base of GUI"""
+    def __init__(self, *args, **kwargs):
+        super(Window, self).__init__(*args, **kwargs)
+
+        self.basicGUI()
+
+    def basicGUI(self):
+        menu = wx.MenuBar()
+        file_button = wx.Menu()
+        exitItem = file_button.Append(wx.ID_EXIT, 'Exit', 'Status msg')
+        menu.Append(file_button, 'File')
+        self.SetMenuBar(menu)
+        self.Bind(wx.EVT_MENU, self.Quit(), exitItem)
+        self.SetTitle('VCTrack')
+        self.Show(True)
+
+    def Quit(self):
+        # Add more here
+        self.Close()
+
+
 if __name__ == '__main__':
     app = wx.App()
-    window = wx.Frame(None, -1, 'VCTrack')
-    window.Show()
+    window = Window(None)
     app.MainLoop()
+
     # data = DataReader.read_data('vocal1DeepCut_resnet50_vocal_strobeMay8shuffle1_200000.h5')
     # t = Tracker(data)
     # t.frame_by()
