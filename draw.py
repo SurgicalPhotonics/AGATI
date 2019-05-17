18e9adfdf8c5ba8782df5f427434f46afd7c2583
Nathaniel Adamian <nat.adamian241@gmail.com>
2019-05-17 16:12:24 -0400

    I lied

---
+++ draw.py
@@ -0,0 +1,8 @@
+import cv2
+# This is basically just notes at this point, also might go back into other file
+
+def draw(img, midline, ac1):
+"""Draws lines on frames of video"""
+    pt1 = midline.
+    img = cv2.line(img, )
+
