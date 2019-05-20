bc6a682ea28d8abd46eba7b0cf70d28edaaf8413
Nathaniel Adamian <nat.adamian241@gmail.com>
2019-05-20 10:58:57 -0400

    Updated draw method

--- draw.py
+++ draw.py
@@ -1,8 +1,10 @@
 import cv2
 # This is basically just notes at this point, also might go back into other file
 
-def draw(img, midline, ac1):
-"""Draws lines on frames of video"""
-    pt1 = midline.
-    img = cv2.line(img, )
+
+def draw(img, midline, ac1, height):
+    """Draws lines on frames of video. Height = window.GetMaxHeight"""
+    pt1 = ((ac1.y - midline.yint) / midline.slope, 0)
+    pt2 = ((height - midline.yint) / midline.slope, height)
+    return cv2.line(img, pt1, pt2, (255, 0, 0), 3)
 
