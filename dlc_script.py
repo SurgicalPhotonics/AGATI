6348a01be75531e5464578f383004296777dd42d
Nathaniel Adamian <nat.adamian241@gmail.com>
2019-05-21 09:41:27 -0400

    Updated dlc scripts. Will implement when big pc is back up and running.

--- dlc_script.py
+++ dlc_script.py
@@ -1,16 +1,57 @@
 import deeplabcut as dlc
+import os
+
+# Only works on windows rn
 
 
-def run_dlc(path):
+def new_proj(path):
+    """Creates new deeplabcut project"""
     project = input('Please input the name of the project (no spaces)')
-    name = input('Enter your own name or that of the principal investigator (no spaces)')
-    dlc.create_new_project(project, name, path)
-    dlc.extract_frames(path)
-    dlc.label_frames(path)
-    # Find way to change label options in config
-    dlc.check_labels(path)
+    name = input('Enter your own name or that of the principal investigator '
+                 '(no spaces)')
+    dlc.create_new_project(project, name, 'r\'' + path)
 
+
+def add_vid(cfg, path):
+    """Adds video(s) to existing dlc project"""
+    if os.path.isdir(path):
+        input('Enter the filetype of the video you want analyzed. ex: \'.mp4\'')
+        dlc.add_new_videos(cfg, '[r\'' + path + '\\' + ']')
+    elif os.path.isfile(path):
+        dlc.add_new_videos(cfg, 'r\'' + path)
+    label_vids(cfg)
+
+
+def label_vids(cfg):
+    """Extracts frames and opens frame labelling GUI"""
+    dlc.extract_frames(cfg)
+    dlc.label_frames(cfg)
+    dlc.create_training_dataset(cfg)
+    dlc.check_labels(cfg)
+
+
+def train(cfg):
+    """Trains network."""
+    dlc.train_network(cfg)
+    dlc.evaluate_network(cfg)
+
+
+def create_vids(cfg, path):
+    """Labels chosen videos with dlc labels."""
+    if os.path.isdir(path):
+        dlc.analyze_videos(cfg, '[' + 'r\'' + path + '\\' + ']')
+        dlc.create_labeled_video(cfg, '[' + 'r\'' + path + '\\' + ']')
+    elif os.path.isfile(path):
+        dlc.analyze_videos(cfg, 'r\'' + path)
+        dlc.create_labeled_video(cfg, 'r\'' + path)
+
+
+def refine(cfg):
+    """Allows users to refine frames."""
+    dlc.refine_labels(cfg)
+    dlc.check_labels(cfg)
+    dlc.merge_datasets(cfg)
+    
 
 if __name__ == '__main__':
-    run_dlc('vocal0DeepCut_resnet50_vocal_strobeMay8shuffle1_200000.h5')
-    # All placeholder stuff, will add to created conda env eventually, but for now code runs on direct paths
+    new_proj('vocal0DeepCut_resnet50_vocal_strobeMay8shuffle1_200000.h5')
