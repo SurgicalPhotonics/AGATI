92e2a2dc6a88c54921204c2853182a58b5cac5f5
Nathaniel Adamian <nat.adamian241@gmail.com>
2019-05-22 11:50:04 -0400

    Can't test draw yet. This is preliminary version

--- draw.py
+++ draw.py
@@ -1,16 +1,21 @@
 import cv2
 # This is basically just notes at this point, also might go back into other file
+VID_FR = 30
 
 
 def draw(path, midline, ac1, height):
     cap = cv2.VideoCapture(path)
     s, im = cap.read()
     count = 0
+    w = cv2.VideoWriter(path, VID_FR)
+    # New path
     while s:
         cv2.imwrite(path, dline(im, midline, ac1, height))
+        w.write(im)
         s, im = cap.read()
         count += 1
-        
+    w.release()
+
 
 def dline(img, midline, ac1, height):
     """Draws lines on frames of video. Height = window.GetMaxHeight"""
