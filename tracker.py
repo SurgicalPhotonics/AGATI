26417a5272fabd3b7807d46ef503998f5bbba55a
Nathaniel Adamian <nat.adamian241@gmail.com>
2019-06-05 14:31:34 -0400

    Added new vocal1 for testing

--- tracker.py
+++ tracker.py
@@ -141,7 +141,7 @@ def calc_reg_line(pt_lst, comm):
         if 3 < len(pfx):
             pf = pfc
     pf = outlier_del(pfx, pfy, comm, pf)
-    if pf[2] ** 2 < .9:
+    if pf[2] ** 2 < .8:
         return None
     slope = pf[0]
     yint = pf[1]
@@ -171,8 +171,8 @@ def outlier_del(pfx, pfy, comm, pf):
 
 
 if __name__ == '__main__':
-    data = read_data('vocalDeepCut_resnet50_vocal_paperMay23shuffle1_900000.h5')
+    data = read_data('vocal1DeepCut_resnet50_vocal_paperMay23shuffle1_900000.h5')
     #data = read_data('uvfp1DeepCut_resnet50_vocal_paperMay23shuffle1_900000.h5')
     t = Tracker(data)
-    t.frame_by('vocalDeepCut_resnet50_vocal_paperMay23shuffle1_900000_labeled.mp4')
+    t.frame_by('vocal1DeepCut_resnet50_vocal_paperMay23shuffle1_900000_labeled.mp4')
     #t.frame_by('uvfp1DeepCut_resnet50_vocal_paperMay23shuffle1_900000_labeled.mp4')
